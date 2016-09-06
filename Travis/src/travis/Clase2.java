a1389a6331f75b05e9733b854671b0272dfa6d04
Alberto <arnalpal95@gmail.com>
2016-09-05 20:19:30 -0500

    Correcion de la clase 2

--- Travis/src/travis/Clase2.java
+++ Travis/src/travis/Clase2.java
@@ -1,6 +1,8 @@
 
 public class Clase2 {
-  private String Valor;
-  
-  System.out.println("Hola" + Valor);
+    private String Valor;
+    public void Msj(){
+        
+        System.out.println("Hola" + Valor);
+    }
 }
